f099eb7df0fdd8a9ea991c209802929efbdde0a3
lyndychivs <lyndychivs@gmail.com>
2025-05-04 22:47:00 +0100

    add 2015 day4

---
+++ AdventOfCode/Year2015/Day4.cs
@@ -0,0 +1,46 @@
+﻿namespace AdventOfCode.Year2015
+{
+    using System;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    public class Day4
+    {
+        public string GetSecretKeyOfHashStartingWithFiveZeros(string input)
+        {
+            int i = 0;
+            while (true)
+            {
+                string hash = GetHash(input, i);
+
+                if (hash.StartsWith("00000"))
+                {
+                    return i.ToString();
+                }
+
+                i++;
+            }
+        }
+
+        public string GetSecretKeyOfHashStartingWithSixZeros(string input)
+        {
+            int i = 0;
+            while (true)
+            {
+                string hash = GetHash(input, i);
+
+                if (hash.StartsWith("000000"))
+                {
+                    return i.ToString();
+                }
+
+                i++;
+            }
+        }
+
+        private string GetHash(string input, int i)
+        {
+            return Convert.ToHexString(MD5.HashData(Encoding.UTF8.GetBytes(input + i)));
+        }
+    }
+}
