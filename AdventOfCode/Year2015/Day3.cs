690cd44ef8d1d6a22bd32ff1856cef075fc33e96
lyndychivs <lyndychivs@gmail.com>
2025-05-04 21:56:35 +0100

    add 2015 day3

---
+++ AdventOfCode/Year2015/Day3.cs
@@ -0,0 +1,98 @@
+﻿namespace AdventOfCode.Year2015
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Day3
+    {
+        public int CalulateHousesVisited(string input)
+        {
+            var houses = new Dictionary<(int, int), House>();
+
+            var currentHouse = new House(0, 0);
+            houses.Add((currentHouse.X, currentHouse.Y), currentHouse);
+
+            foreach (char direction in input)
+            {
+                currentHouse = GetNextHouse(direction, currentHouse);
+
+                if (houses.ContainsKey((currentHouse.X, currentHouse.Y)))
+                {
+                    houses[(currentHouse.X, currentHouse.Y)].Visits++;
+                    continue;
+                }
+
+                houses.Add((currentHouse.X, currentHouse.Y), new House(currentHouse.X, currentHouse.Y));
+            }
+
+            return houses.Count;
+        }
+
+        public int CalulateHousesVisitedBySantaAndRobot(string input)
+        {
+            var houses = new Dictionary<(int, int), House>();
+
+            var santaHouse = new House(0, 0);
+            var robotHouse = new House(0, 0);
+
+            houses.Add((santaHouse.X, santaHouse.Y), santaHouse);
+            houses[(0, 0) ].Visits++;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char direction = input[i];
+                if (i % 2 == 0)
+                {
+                    santaHouse = GetNextHouse(direction, santaHouse);
+
+                    if (houses.ContainsKey((santaHouse.X, santaHouse.Y)))
+                    {
+                        houses[(santaHouse.X, santaHouse.Y)].Visits++;
+                        continue;
+                    }
+
+                    houses.Add((santaHouse.X, santaHouse.Y), new House(santaHouse.X, santaHouse.Y));
+                }
+                else
+                {
+                    robotHouse = GetNextHouse(direction, robotHouse);
+
+                    if (houses.ContainsKey((robotHouse.X, robotHouse.Y)))
+                    {
+                        houses[(robotHouse.X, robotHouse.Y)].Visits++;
+                        continue;
+                    }
+
+                    houses.Add((robotHouse.X, robotHouse.Y), new House(robotHouse.X, robotHouse.Y));
+                }
+            }
+
+            return houses.Count;
+        }
+
+        private House GetNextHouse(char direction, House house)
+        {
+            switch(direction)
+            {
+                case '>':
+                    return new House(house.X + 1, house.Y);
+                case '<':
+                    return new House(house.X - 1, house.Y);
+                case '^':
+                    return new House(house.X, house.Y + 1);
+                case 'v':
+                    return new House(house.X, house.Y - 1);
+                default:
+                    throw new ArgumentException($"Invalid direction: {direction}");
+            }
+        }
+    }
+
+    public class House(int x, int y)
+    {
+        public int X { get; private set; } = x;
+        public int Y { get; private set; } = y;
+
+        public int Visits = 1;
+    }
+}
