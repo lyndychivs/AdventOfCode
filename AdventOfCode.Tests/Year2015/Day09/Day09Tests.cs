d6d7645d9fe8b53a8aa73879d977b875d7b963dd
lyndychivs <lyndychivs@gmail.com>
2025-05-07 02:13:20 +0100

    add 2015 day9

---
+++ AdventOfCode.Tests/Year2015/Day09/Day09Tests.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCode.Tests.Year2015.Day09
+{
+    using AdventOfCode.Year2015.Day09;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Day09Tests
+    {
+        private const string InputFile = "Year2015/Day09/input.txt";
+
+        private const string ExampleFile = "Year2015/Day09/example.txt";
+
+        [Test]
+        public void Day09_Part1()
+        {
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(new Part1().CalculateShortestRouteDistance(FileOperations.GetInputFileLines(ExampleFile)), Is.EqualTo(0));
+
+                Assert.That(new Part1().CalculateShortestRouteDistance(FileOperations.GetInputFileLines(InputFile)), Is.EqualTo(0));
+            }
+        }
+    }
+}
---
+++ AdventOfCode/Year2015/Day09/Part1.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode.Year2015.Day09
+{
+    using System.Collections.Generic;
+
+    public class Part1
+    {
+        public int CalculateShortestRouteDistance(IEnumerable<string> inputs)
+        {
+            return 0;
+        }
+    }
+}
