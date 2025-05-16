76d047f41dc135c86a531c817d6fe1a6ee7bb359
lyndychivs <lyndychivs@gmail.com>
2025-05-16 23:12:02 +0100

    add 2016 day07

---
+++ AdventOfCode.Tests/Year2016/Day07/Day07Tests.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCode.Tests.Year2016.Day07
+{
+    using AdventOfCode.Year2016.Day07;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Day07Tests
+    {
+        private const string ExampleFilePath = "Year2016/Day07/example.txt";
+
+        private const string InputFilePath = "Year2016/Day07/input.txt";
+
+        [Test]
+        public void Day07_Part1()
+        {
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(new Part1().GetCountOfValidIpAddresses(FileOperations.GetInputFileLines(ExampleFilePath)), Is.EqualTo(2));
+
+                Assert.That(new Part1().GetCountOfValidIpAddresses(FileOperations.GetInputFileLines(InputFilePath)), Is.EqualTo(115));
+            }
+        }
+    }
+}
---
+++ AdventOfCode/Year2016/Day07/Part1.cs
@@ -0,0 +1,64 @@
+﻿namespace AdventOfCode.Year2016.Day07
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Part1
+    {
+        public int GetCountOfValidIpAddresses(IEnumerable<string> inputs)
+        {
+            int count = 0;
+            foreach (string input in inputs)
+            {
+                if (IsValidIpAddress(input))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private bool IsValidIpAddress(string input)
+        {
+            string[] parts = input.Split(['[', ']'], StringSplitOptions.RemoveEmptyEntries);
+
+            bool response = false;
+            for (int i = 0; i < parts.Length; i++)
+            {
+                bool doesContainPalindrome = DoesContainPalindrome(parts[i]);
+
+                if (doesContainPalindrome && i % 2 == 0 == false)
+                {
+                    return false;
+                }
+
+                if (doesContainPalindrome)
+                {
+                    response = true;
+                }
+            }
+
+            return response;
+        }
+
+        private bool DoesContainPalindrome(string part)
+        {
+            for (int i = 0; i < part.Length - 3; i++)
+            {
+                if (part[i] == part[i + 3]
+                    && part[i + 1] == part[i + 2])
+                {
+                    if (part[i] == part[i + 1])
+                    {
+                        return false;
+                    }
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
