191814a5def212ae3647d1ee0701135d9ed1df6c
lyndychivs <lyndychivs@gmail.com>
2025-05-17 16:23:26 +0100

    add 2016 day7

--- AdventOfCode.Tests/Year2016/Day07/Day07Tests.cs
+++ AdventOfCode.Tests/Year2016/Day07/Day07Tests.cs
@@ -9,6 +9,8 @@
     {
         private const string ExampleFilePath = "Year2016/Day07/example.txt";
 
+        private const string ExampleTwoFilePath = "Year2016/Day07/example2.txt";
+
         private const string InputFilePath = "Year2016/Day07/input.txt";
 
         [Test]
@@ -19,6 +21,17 @@
                 Assert.That(new Part1().GetCountOfValidIpAddresses(FileOperations.GetInputFileLines(ExampleFilePath)), Is.EqualTo(2));
 
                 Assert.That(new Part1().GetCountOfValidIpAddresses(FileOperations.GetInputFileLines(InputFilePath)), Is.EqualTo(115));
+            }
+        }
+
+        [Test]
+        public void Day07_Part2()
+        {
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(new Part2().GetCountOfValidIpAddresses(FileOperations.GetInputFileLines(ExampleTwoFilePath)), Is.EqualTo(3));
+
+                Assert.That(new Part2().GetCountOfValidIpAddresses(FileOperations.GetInputFileLines(InputFilePath)), Is.EqualTo(231));
             }
         }
     }
---
+++ AdventOfCode/Year2016/Day07/Part2.cs
@@ -0,0 +1,71 @@
+﻿namespace AdventOfCode.Year2016.Day07
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Part2
+    {
+        public int GetCountOfValidIpAddresses(IEnumerable<string> inputs)
+        {
+            int count = 0;
+            foreach (string input in inputs)
+            {
+                if (IsValidIpAddress(input))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private bool IsValidIpAddress(string input)
+        {
+            string[] parts = input.Split(['[', ']'], StringSplitOptions.RemoveEmptyEntries);
+
+            var palindromesInternal = new List<string>();
+            var palindromesExternal = new List<string>();
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    palindromesExternal.AddRange(GetPalindromes(parts[i]));
+                }
+                else
+                {
+                    palindromesInternal.AddRange(GetPalindromes(parts[i]));
+                }
+            }
+
+            foreach (string palindrome in palindromesInternal)
+            {
+                if (palindromesExternal.Contains($"{palindrome[1]}{palindrome[0]}{palindrome[1]}"))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private List<string> GetPalindromes(string part)
+        {
+            var response = new List<string>();
+            for (int i = 0; i < part.Length - 2; i++)
+            {
+                if (part[i] == part[i + 2])
+                {
+                    if (part[i] == part[i + 1])
+                    {
+                        continue;
+                    }
+
+                    response.Add($"{part[i]}{part[i + 1]}{part[i + 2]}");
+                }
+            }
+
+            return response;
+        }
+    }
+}
