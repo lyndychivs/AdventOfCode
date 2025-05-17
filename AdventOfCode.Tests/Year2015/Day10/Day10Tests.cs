b3a1ef2349a9a0ba9bfe08c402a358343ee39ce5
lyndychivs <lyndychivs@gmail.com>
2025-05-17 17:33:38 +0100

    add 2015 day10

---
+++ AdventOfCode.Tests/Year2015/Day10/Day10Tests.cs
@@ -0,0 +1,24 @@
+﻿namespace AdventOfCode.Tests.Year2015.Day10
+{
+    using AdventOfCode.Year2015.Day10;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Day10Tests
+    {
+        private const string Input = "1321131112";
+
+        [Test]
+        public void Day10_Part1()
+        {
+            Assert.That(new Part1().Solve(Input), Is.EqualTo(492982));
+        }
+
+        [Test]
+        public void Day10_Part2()
+        {
+            Assert.That(new Part2().Solve(Input), Is.EqualTo(6989950));
+        }
+    }
+}
---
+++ AdventOfCode/Year2015/Day10/Part1.cs
@@ -0,0 +1,61 @@
+﻿namespace AdventOfCode.Year2015.Day10
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Part1
+    {
+        public int Solve(string input)
+        {
+            for (int i = 0; i < 40; i++)
+            {
+                input = LookAndSay(input);
+            }
+
+            return input.Length;
+        }
+
+        private string LookAndSay(string input)
+        {
+            var pairs = new List<Pair>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+
+                int currentCharCount = 0;
+                while (i + currentCharCount < input.Length && input[i + currentCharCount] == currentChar)
+                {
+                    currentCharCount++;
+                }
+
+                pairs.Add(new Pair(currentChar, currentCharCount));
+
+                i += currentCharCount - 1;
+            }
+
+            var response = new StringBuilder();
+            foreach (Pair pair in pairs)
+            {
+                _ = response.Append(pair.ToString());
+            }
+
+            Console.WriteLine(response.ToString());
+
+            return response.ToString();
+        }
+
+        private class Pair(char Character, int Count)
+        {
+            public char Character { get; } = Character;
+
+            public int Count { get; } = Count;
+
+            public override string ToString()
+            {
+                return $"{Count}{Character}";
+            }
+        }
+    }
+}
