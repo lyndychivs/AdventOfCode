90ff23234cb6fe0b18f926fc8167aeff2e934821
lyndychivs <lyndychivs@gmail.com>
2025-05-20 18:41:49 +0100

    add 2015 day11

---
+++ AdventOfCode.Tests/Year2015/Day11/Day11Tests.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode.Tests.Year2015.Day11
+{
+    using AdventOfCode.Year2015.Day11;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Day11Tests
+    {
+        private const string Input = "hxbxwxba";
+
+        [Test]
+        public void Day11_Part1()
+        {
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(new Part1().GetNextPassword("abcdefgh"), Is.EqualTo("abcdffaa"));
+
+                Assert.That(new Part1().GetNextPassword("ghijklmn"), Is.EqualTo("ghjaabcc"));
+
+                Assert.That(new Part1().GetNextPassword(Input), Is.EqualTo("hxbxxyzz"));
+            }
+        }
+
+        [Test]
+        public void Day11_Part2()
+        {
+            Assert.That(new Part1().GetNextPassword("hxbxxyzz"), Is.EqualTo("hxcaabcc"));
+        }
+    }
+}
---
+++ AdventOfCode/Year2015/Day11/Part1.cs
@@ -0,0 +1,113 @@
+﻿namespace AdventOfCode.Year2015.Day11
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Part1
+    {
+        private readonly List<char> _forbiddenChars = [ 'i', 'o', 'l' ];
+
+        public string GetNextPassword(string input)
+        {
+            do
+            {
+                input = IncrementPassword(input);
+            }
+            while (!IsPasswordValid(input));
+
+            return input;
+        }
+
+        private string IncrementPassword(string password)
+        {
+            char c = GetNextChar(password[^1]);
+
+            string newPassword = string.Empty;
+            if (c == 'a')
+            {
+                newPassword = IncrementPassword(password[..^1]);
+            }
+
+            return (string.IsNullOrWhiteSpace(newPassword) ? password[..^1] : newPassword) + c;
+        }
+
+        private bool IsPasswordValid(string password)
+        {
+            if (password.Length != 8)
+            {
+                return false;
+            }
+
+            return DoesContainTwoUniquePairs(password) && DoesContainStraight(password) && !ContainsForbiddenChars(password);
+        }
+
+        private bool ContainsForbiddenChars(string password)
+        {
+            foreach (char forbiddenChar in _forbiddenChars)
+            {
+                if (password.Contains(forbiddenChar))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool DoesContainTwoUniquePairs(string password)
+        {
+            List<char> pairs = [];
+
+            for (int i = 0; i < password.Length - 1; i++)
+            {
+                if (IsPair(password[i], password[i + 1]))
+                {
+                    pairs.Add(password[i]);
+                    i++;
+                }
+            }
+
+            return pairs.Distinct().ToList().Count >= 2;
+        }
+
+        private bool IsPair(char c1, char c2)
+        {
+            return c1 == c2;
+        }
+
+        private bool DoesContainStraight(string password)
+        {
+            for (int i = 0; i < password.Length - 2; i++)
+            {
+                if (IsStraight(password[i], password[i + 1], password[i + 2]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsStraight(char c1, char c2, char c3)
+        {
+            return c1 + 1 == c2 && c2 + 1 == c3;
+        }
+
+        private char GetNextChar(char c)
+        {
+            if (c == 'z')
+            {
+                return 'a';
+            }
+
+            char nextChar = (char)(c + 1);
+
+            if (_forbiddenChars.Contains(nextChar))
+            {
+                return GetNextChar(nextChar);
+            }
+
+            return nextChar;
+        }
+    }
+}
